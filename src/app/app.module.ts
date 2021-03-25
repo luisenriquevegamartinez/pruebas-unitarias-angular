89fc28baefedef37afeaca5fb7f51aeddfa842bb
LuisVega91 <luisevegam.91@gmail.com>
2021-03-25 01:07:12 -0500

    se agregan pruebas de integracion
    se agregan mas pruebas de integracion y se aprende a separar pruebas de integracino de las pruebas unitarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
 import { MedicoService } from './intermedio2/medico/medico.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,16 +7,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
     MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [MedicoService],
   bootstrap: [AppComponent]
