cca641f8edca433c4e7907d7c522d3da70916b4a
LuisVega91 <luisevegam.91@gmail.com>
2021-03-26 08:17:48 -0500

    add.

---
+++ src/app/avanzadas/rutas/app.routes.spec.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { HospitalComponent } from '../../intermedio2/hospital/hospital.component';
+
+
+export const RUTAS: Routes = [
+  { path: 'hospital', component: HospitalComponent }
+]
